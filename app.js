7996c51eeadafca907171aeeff2e67f2bbafd466
SueAkira <Akira@SueAkiradeMacBook-Air.local>
2017-07-28 14:29:07 -0700

    trying now ahhhhh

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 // globals
 
-var keyword = "new york";
+
+$("#submit-search-info").on("click", function(event) {
+
+event.preventDefault();
+	
+$("#articles").empty();
+
+var keyword = $("#search-term").val();
 var key = "api-key=d5fb0ee0ea2a4439a2a4f5ad63f1f268";
-var queryurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&" + key;
+var startyear = "start_year=" + $("#start-year").val().trim();
+var endyear = "end_year=" + $("#end-year").val().trim();
+var number = $("#numRecordsSelect").val();
+var queryurl;
 
-// placeholders
-var begindate;
-var enddate;
+if ($("#start-year").val().trim().length === 4 && $("#end-year").val().trim() === 4) {
+	queryurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&" + key + "&" + startyear + "&" + endyear;
+}
+else if ($("#start-year").val().trim().length === 4 && $("#end-year").val().trim() !== 4) {
+	queryurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&" + key + "&" + startyear;
+}
+else if ($("#start-year").val().trim().length !== 4 && $("#end-year").val().trim() === 4) {
+	queryurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&" + key  + "&" + endyear;
+}
+else {
+	queryurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&" + key;
+}
+
 var numofarticles = 5;
-
 
 $.ajax({
 	url: queryurl,
@@ -17,14 +36,26 @@ $.ajax({
 	for (i = 0; i < numofarticles; i++) {
 		var result = object.response;
 		var author = result.docs[i].byline.original;
-		console.log(author);
 		var date = result.docs[i].pub_date;
-		console.log(date);
 		var section = result.docs[i].section_name;
-		console.log(section);
 		var title = result.docs[i].headline.main;
 		var link = result.docs[i].web_url;
 		// console.log(title);
 		// console.log(link);
+		var indexlabel = $("<span class='label label-primary'>").html(i + 1);
+		var newdiv = $("<div class='well'>");
+		var titleHead = $("<h1>").html(title).prepend(indexlabel);
+		var authorline = $("<p>").html(author);
+		var dateline = $("<p>").html(date);
+		var sectionline = $("<p>").html(section);
+		var linkline = $("<a>").attr("href", link).text(link);
+		newdiv.append(titleHead, authorline, dateline, sectionline, linkline);
+		$("#articles").append(newdiv);
+
 	}
+})
+})
+
+$("#clear-results").on("click", function() {
+	$("#articles").empty();
 })
